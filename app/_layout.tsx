51c25cd66f1fe3fc28ba64cec71b41f80ea0c76a
Sarang T K <sarangtk2000@gmail.com>
2026-02-03 20:12:54 +0530

    fixed an eslint issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { QueryClientProvider } from '@tanstack/react-query';
-import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+import { Stack } from 'expo-router';
 
 import { queryClient } from '@/lib/queryClient';
 
